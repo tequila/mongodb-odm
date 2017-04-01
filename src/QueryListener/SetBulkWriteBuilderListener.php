e3409feb5e60bf35a7ae000c484633d8d505a675
Petr Buchin <petrbuchyn@gmail.com>
2017-04-02 01:18:27 +0300

    Fixed namespace in SetBulkWriteBuilderListener

--- src/QueryListener/SetBulkWriteBuilderListener.php
+++ src/QueryListener/SetBulkWriteBuilderListener.php
@@ -1,8 +1,10 @@
 <?php
 
-namespace Tequila\MongoDB\ODM;
+namespace Tequila\MongoDB\ODM\QueryListener;
 
 use Tequila\MongoDB\DocumentListenerInterface;
+use Tequila\MongoDB\ODM\BulkWriteBuilderAwareInterface;
+use Tequila\MongoDB\ODM\BulkWriteBuilderFactory;
 use Tequila\MongoDB\QueryCursor;
 use Tequila\MongoDB\QueryListenerInterface;
 
